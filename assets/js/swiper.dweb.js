4f9b18558823d30533ce5ff3c5a6e684b40a1cb5
skyadrum <mmcready07@gmail.com>
2018-05-31 16:44:28 -0500

    Se crea disparador de swiper para la seccion dweb.html

---
+++ assets/js/swiper.dweb.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      loop: true,
+      speed: 500,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 7000,
+      }
+    });
